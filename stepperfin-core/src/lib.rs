6c7f5fac27945549eb35b0fbef73e8a39fb16072
Michael Bryan <michaelfbryan@gmail.com>
2019-03-08 20:16:10 +0800

    Made a simple device trait

--- stepperfin-core/src/lib.rs
+++ stepperfin-core/src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+
+use uom::si::f32::{Length, Velocity};
+
+/// The basic abstraction over device hardware.
+pub trait Printer {
+    fn axis(&self, letter: char) -> Option<&dyn LinearAxis>;
+    fn axis_mut(&self, letter: char) -> Option<&mut dyn LinearAxis>;
+}
+
+pub trait LinearAxis {
+    /// The current axis position.
+    fn current_position(&self) -> Length;
+    /// Reset the axis position (e.g. after doing a calibration sequence).
+    fn reset_position(&mut self, new_pos: Length);
+    /// Set the desired velocity.
+    fn set_velocity(&mut self, velocity: Velocity);
+
+    /// Has this axis hit a limit switch (if connected)?
+    fn limit_switch_triggered(&self) -> bool {
+        false
     }
 }
