ef4c6debf52823212d98b658c6ba6691620a5689
Michael Bryan <michaelfbryan@gmail.com>
2019-03-09 07:53:48 +0800

    Made `Task` more flexible

--- stepperfin-core/src/tasks.rs
+++ stepperfin-core/src/tasks.rs
@@ -1,5 +1,9 @@
-/// An arbitrary operation which can be periodically polled to completion.
-pub trait Task<State, Triggers> {
+/// An operation which can be polled to completion.
+pub trait Task<State, Triggers>
+where
+    State: ?Sized,
+    Triggers: ?Sized,
+{
     /// Make progress on the task.
     fn tick(&mut self, state: &State, triggers: &mut Triggers) -> Continuation;
 }
@@ -12,4 +16,28 @@ pub enum Continuation {
     Faulted,
     /// The task has run to completion.
     Complete,
+}
+
+impl<F, State, Triggers> Task<State, Triggers> for F
+where
+    F: FnMut(&State, &mut Triggers) -> Continuation,
+{
+    fn tick(&mut self, state: &State, triggers: &mut Triggers) -> Continuation {
+        self(state, triggers)
+    }
+}
+
+pub fn wait_until<F, State, Triggers>(
+    mut predicate: F,
+) -> impl Task<State, Triggers>
+where
+    F: FnMut(&State) -> bool,
+{
+    move |state: &State, _triggers: &mut Triggers| {
+        if predicate(state) {
+            Continuation::Complete
+        } else {
+            Continuation::Continue
+        }
+    }
 }
