f933fe1bf81955dd0e90e23a9dc4061031a15d4e
Michael Bryan <michaelfbryan@gmail.com>
2019-03-10 19:12:42 +0800

    Made a LED roulette

--- stepperfin-stm32f3-discovery/src/main.rs
+++ stepperfin-stm32f3-discovery/src/main.rs
@@ -1,18 +1,39 @@
 #![no_std]
 #![no_main]
 
+#[cfg(not(test))]
 extern crate panic_halt;
-extern crate stm32f30x_hal;
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
 
 use cortex_m_rt::entry;
+use f3::led::Leds;
+use stm32f30x_hal::prelude::*;
+use stm32f30x_hal::stm32f30x;
+use stm32f30x_hal::delay::Delay;
 
-// use `main` as the entry point of this application
-// `main` is not allowed to return
 #[entry]
 fn main() -> ! {
-    // initialization
+    let core_peripherals = cortex_m::Peripherals::take().unwrap();
+    let device_peripherals = stm32f30x::Peripherals::take().unwrap();
 
-    loop {
-        // application logic
+    let mut flash = device_peripherals.FLASH.constrain();
+    let mut rcc = device_peripherals.RCC.constrain();
+    let gpioe = device_peripherals.GPIOE.split(&mut rcc.ahb);
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut leds = Leds::new(gpioe);
+    let mut delay = Delay::new(core_peripherals.SYST, clocks);
+
+    loop { 
+        for i in 0..leds.len() {
+            leds[i].off();
+            let next = (i+1) % leds.len();
+            leds[next].on();
+            delay.delay_ms(100_u8);
+        }
     }
 }
